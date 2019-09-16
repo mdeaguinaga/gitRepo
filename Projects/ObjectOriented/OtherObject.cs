da8958bf98f89d7c31741c52c71bf0a9573ac75b
Manuel <isra_de@outlook.com>
2019-09-16 15:01:19 -0500

    first commit

---
+++ Projects/ObjectOriented/OtherObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    public struct EmptyStruct
+    {
+    }
+
+}
+
+
+class OtherObject
+{
+    public int classNumber;
+
+    public int TheNumber(int number)
+    {
+        this.classNumber = number + 25;
+        return classNumber;
+    }
+}
